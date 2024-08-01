438bcc9d6c828e5b761599bfba9d9a22e509f122
IssaMDounejjaR <otaku6issam@gmail.com>
2024-08-01 20:02:41 +0100

    handle disconnected

--- client.ts
+++ client.ts
@@ -1,9 +1,5 @@
 import * as common from "./common";
 
-const ws = new WebSocket(`ws://localhost:${common.PORT}/`);
-
-ws.binaryType = "arraybuffer";
-
 (async function () {
   const canvas = document.getElementById("game") as HTMLCanvasElement | null;
 
@@ -13,111 +9,127 @@ ws.binaryType = "arraybuffer";
 
   if (!ctx) throw new Error("No support for 2d");
 
-  const joinedPlayers = new Map<number, common.Player>();
-
   ctx.canvas.width = common.GAME_WIDTH;
   ctx.canvas.height = common.GAME_HEIGHT;
 
-  ws.addEventListener("open", () => {
-    let me: common.Player | undefined;
+  let ws: WebSocket | undefined = new WebSocket(
+    `ws://localhost:${common.PORT}/`,
+  );
 
-    ws.addEventListener("message", (e: MessageEvent) => {
-      // console.log(e.data);
-      // if (e.data instanceof ArrayBuffer) {
-      //   const view = new DataView(e.data);
-      //
-      //   console.log(view.getUint8(0));
-      // }
-      // return;
-      const data = JSON.parse(e.data);
+  let me: common.Player | undefined;
 
-      if (common.isWelcome(data)) {
-        me = {
-          id: data.id,
-          x: data.x,
-          y: data.y,
-          moving: data.moving,
-          hue: data.hue,
-        };
+  ws.binaryType = "arraybuffer";
 
-        joinedPlayers.set(data.id, me);
-      } else if (common.isPlayerJoined(data)) {
-        joinedPlayers.set(data.id, {
-          id: data.id,
-          x: data.x,
-          y: data.y,
-          moving: {
-            left: false,
-            right: false,
-            up: false,
-            down: false,
-          },
-          hue: data.hue,
+  const joinedPlayers = new Map<number, common.Player>();
+
+  ws.addEventListener("open", () => { });
+
+  ws.addEventListener("message", (e: MessageEvent) => {
+    // console.log(e.data);
+    // if (e.data instanceof ArrayBuffer) {
+    //   const view = new DataView(e.data);
+    //
+    //   console.log(view.getUint8(0));
+    // }
+    // return;
+    const data = JSON.parse(e.data);
+
+    if (common.isWelcome(data)) {
+      me = {
+        id: data.id,
+        x: data.x,
+        y: data.y,
+        moving: data.moving,
+        hue: data.hue,
+      };
+
+      joinedPlayers.set(data.id, me);
+    } else if (common.isPlayerJoined(data)) {
+      joinedPlayers.set(data.id, {
+        id: data.id,
+        x: data.x,
+        y: data.y,
+        moving: {
+          left: false,
+          right: false,
+          up: false,
+          down: false,
+        },
+        hue: data.hue,
+      });
+    } else if (common.isPlayerMoving(data)) {
+      const player = joinedPlayers.get(data.id);
+
+      if (player) {
+        player.x = data.x;
+        player.y = data.y;
+        player.moving[data.direction] = data.start;
+      }
+    } else if (common.isPlayerLeft(data)) {
+      joinedPlayers.delete(data.id);
+    } else {
+      console.error("Unknown message: ", data);
+    }
+  });
+
+  ws.addEventListener("close", () => {
+    ws = undefined;
+  });
+
+  document.addEventListener("keydown", (e: KeyboardEvent) => {
+    if (ws) {
+      const direction = common.DIRECTONS_KEYS[e.key];
+
+      if (direction !== undefined) {
+        common.sendMessage<common.PlayerStartMoving>(ws, {
+          kind: common.MessageKind.PlayerStartMoving,
+          start: true,
+          direction,
         });
-      } else if (common.isPlayerMoving(data)) {
-        const player = joinedPlayers.get(data.id);
-
-        if (player) {
-          player.x = data.x;
-          player.y = data.y;
-          player.moving[data.direction] = data.start;
-        }
-      } else if (common.isPlayerLeft(data)) {
-        joinedPlayers.delete(data.id);
-      } else {
-        console.error("Unknown message: ", data);
       }
-    });
+    }
+  });
 
-    ws.addEventListener("close", () => {
-      joinedPlayers.clear();
-    });
+  document.addEventListener("keyup", (e: KeyboardEvent) => {
+    if (ws) {
+      const direction = common.DIRECTONS_KEYS[e.key];
 
-    document.addEventListener("keydown", (e: KeyboardEvent) => {
-      if (me) {
-        const direction = common.DIRECTONS_KEYS[e.key];
-
-        if (direction !== undefined) {
-          common.sendMessage<common.PlayerStartMoving>(ws, {
-            kind: common.MessageKind.PlayerStartMoving,
-            start: true,
-            direction,
-          });
-        }
+      if (direction !== undefined) {
+        common.sendMessage<common.PlayerStartMoving>(ws, {
+          kind: common.MessageKind.PlayerStartMoving,
+          start: false,
+          direction,
+        });
       }
-    });
+    }
+  });
 
-    document.addEventListener("keyup", (e: KeyboardEvent) => {
-      if (me) {
-        const direction = common.DIRECTONS_KEYS[e.key];
+  let previousTimestamp = 0;
 
-        if (direction !== undefined) {
-          common.sendMessage<common.PlayerStartMoving>(ws, {
-            kind: common.MessageKind.PlayerStartMoving,
-            start: false,
-            direction,
-          });
-        }
-      }
-    });
+  const frame = (timestamp: number) => {
+    const deltaTime = (timestamp - previousTimestamp) / 1000;
+    previousTimestamp = timestamp;
 
-    let previousTimestamp = 0;
+    ctx.fillStyle = "#222";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    const frame = (timestamp: number) => {
-      const deltaTime = (timestamp - previousTimestamp) / 1000;
-      previousTimestamp = timestamp;
+    if (!ws) {
+      const label = "Disconnected";
+      const size = ctx.measureText(label);
 
-      ctx.fillStyle = "#222";
-      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.font = "48px bold";
+      ctx.fillStyle = "white";
 
+      ctx.fillText(
+        label,
+        ctx.canvas.width / 2 - size.width / 2,
+        ctx.canvas.height / 2,
+      );
+    } else {
       joinedPlayers.forEach((player) => {
         common.updatePlayer(player, deltaTime);
 
-        const brightness = player.id === me?.id ? "50%" : "30%";
-
-        ctx.fillStyle = `hsl(${player.hue} 80% ${brightness})`;
-        ctx.strokeStyle = "white";
-        ctx.lineWidth = 3;
+        ctx.fillStyle = `hsl(${player.hue} 70% 40%)`;
 
         ctx.fillRect(
           player.x,
@@ -125,20 +137,21 @@ ws.binaryType = "arraybuffer";
           common.PLAYER_SIZE,
           common.PLAYER_SIZE,
         );
-
-        if (me && player.id === me.id) {
-          ctx.strokeRect(
-            player.x,
-            player.y,
-            common.PLAYER_SIZE,
-            common.PLAYER_SIZE,
-          );
-        }
       });
 
-      window.requestAnimationFrame(frame);
-    };
+      if (me) {
+        ctx.fillStyle = `hsl(${me.hue} 100% 40%)`;
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 3;
+
+        ctx.fillRect(me.x, me.y, common.PLAYER_SIZE, common.PLAYER_SIZE);
+
+        ctx.strokeRect(me.x, me.y, common.PLAYER_SIZE, common.PLAYER_SIZE);
+      }
+    }
 
     window.requestAnimationFrame(frame);
-  });
+  };
+
+  window.requestAnimationFrame(frame);
 })();
